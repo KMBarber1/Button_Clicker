f8045ca92b625167bbb13ab04c5835babf3877cb
katherine barber <katherine.m.barber@outlook.com>
2021-12-22 00:14:43 -0700

    Initial Commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// console.log("Hello world!");
+
+
+function login(element){
+    element.innerText = "logout";
+}
+
+
+function alertWord(){
+    alert("Thank you!");
+}
+
+function removedElement(){
+    var element = document.querySelector("#removedElement");
+    element.remove();
+}
+
